e3df95859b776805c14f260cb7b2fcfd18b75a5e
RaidasGrisk <mrraidas@gmail.com>
2019-09-03 13:05:12 +0300

    update

---
+++ config.py
@@ -0,0 +1,8 @@
+
+CHAR_VECTOR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-"
+
+NUM_CLASSES = len(CHAR_VECTOR) + 1
+
+FLAGS = {'training_data_path': 'C:/Users/Raidas/Desktop/fots/ICDAR15/ch4_training_images',
+         'training_annotation_path': 'C:/Users/Raidas/Desktop/fots/ICDAR15/ch4_training_localization_transcription_gt',
+         'min_text_size': 0}
